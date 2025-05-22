af26eb26c478c09bd97494fb2422653dbf5d89b1
Chris <cj.christopherjude@gmail.com>
2025-05-22 01:58:01 +0100

    UI updates
    -Flex grow footer
    -Hero cards aesthetic

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -4,9 +4,9 @@ import Footer from '@/components/Footer';
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className='min-h-screen flex flex-col'>
       <Navbar />
-      <main>{children}</main>
+      <main className='flex-grow px-5'>{children}</main>
       <Footer />
     </div>
   );
